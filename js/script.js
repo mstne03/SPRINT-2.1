f9fb2b6b15f194edf0599ee1352f3323cf1b4729
mstne03 <mserraitorne@gmail.com>
2025-05-14 23:17:23 +0200

    Task 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 (() => {
     const links = document.querySelectorAll(".nav-link");
     const tasks = document.querySelectorAll(".task");
+    const output1 = document.querySelector(".task__content-answer");
+    const form = document.querySelector(".task__content-form");
+    const randNumber = document.querySelector(".task__content-form-submit-btn--2");
 
     links.forEach(link => {
         link.addEventListener("click", () => {
@@ -16,16 +19,12 @@
         });
     });
 
-    const output = document.querySelector(".task__content-answer");
-    output.style.display = "none";
-    const form = document.querySelector(".task__content-form");
-
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
         let n1 = parseInt(document.querySelector("#num1").value);
         let n2 = parseInt(document.querySelector("#num2").value);
-        const result = document.getElementById("result");
+        const result = document.getElementById("result1");
 
         const sum = (n,m) => n+m;
 
@@ -33,6 +32,14 @@
 
         result.innerHTML = r;
 
-        output.style.display = "flex";
+        output1.style.display = "flex";
+    });
+
+    randNumber.addEventListener("click", () => {
+        let number = Math.floor(Math.random()*101);
+
+        const output2 = document.querySelector("#result2");
+
+        output2.innerHTML = number;
     });
 })();
