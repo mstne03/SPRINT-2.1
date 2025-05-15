01a9ed96f474063cd1d0aec6223efd73d7f9bfa0
mstne03 <mserraitorne@gmail.com>
2025-05-15 13:09:13 +0200

    Task 2

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ const canDrive = (age) =>
 
 console.log(canDrive(12));
 console.log(canDrive(18));
+
+//Task 2
+const compare = (n1,n2) => n1>n2 ? `${n1} is bigger than ${n2}` :
+                           n1<n2 ? `${n1} is smaller than ${n2}` :
+                           `${n1} is equal to ${n2}`;
+console.log(compare(6,6));
