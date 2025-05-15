6c0eb2c6d20bee346901ce4c56c2e5b1aeba27e7
mstne03 <mserraitorne@gmail.com>
2025-05-15 17:31:18 +0200

    Task 4

--- js/script.js
+++ js/script.js
@@ -16,3 +16,15 @@ const numbers = [1,2,3,4,5,6];
 const even = numbers.filter(number => number%2 == 0);
 
 console.log(even);
+
+//Task 4
+const obj = {
+    name: "Ona",
+    age: 25,
+    city: "Barcelona"
+};
+
+for (atr in obj) {
+    console.log(atr+":");
+    console.log(obj[atr]);
+}
