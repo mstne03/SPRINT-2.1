072ffd4852f24376831b4149e262f5b947ef665c
mstne03 <mserraitorne@gmail.com>
2025-05-15 17:19:15 +0200

    Task 6

--- js/script.js
+++ js/script.js
@@ -28,3 +28,9 @@ const array4 = [1,3,7,10,15,17,11,5,8,12,9];
 let result = array4.filter(n => n>=10).map(num => num*2).reduce((total,current) => total+current);
 
 console.log(result);
+
+//Task 6
+const array5 = [11,12,13,14];
+
+console.log(array5.every((n) => n>10));
+console.log(array5.some((n) => n>10));
