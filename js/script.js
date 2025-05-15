0c1526c0f72270fcc5a676c6615aaa3999663c9b
mstne03 <mserraitorne@gmail.com>
2025-05-15 16:21:11 +0200

    Task 2

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ const array2 = [4,5,6];
 const array3 = [...array1,...array2];
 
 console.log(array3);
+
+//Task 2
+const sum = (...n) => {
+
+    let result = 0;
+
+    for (num of n) {
+        num = parseInt(num);
+
+        if (isNaN(num)) {
+            continue;
+        }
+
+        result += num;
+    }
+
+    return result;
+};
+
+console.log(sum(2,3,4,5));
