885edd869ed29282609d81b2818cbf5830aec810
mstne03 <mserraitorne@gmail.com>
2025-05-15 17:27:51 +0200

    Task 3

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ names.forEach(name => console.log(name));
 for (n of names) {
     console.log(n);
 }
+
+//Task 3
+const numbers = [1,2,3,4,5,6];
+
+const even = numbers.filter(number => number%2 == 0);
+
+console.log(even);
